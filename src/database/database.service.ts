6781a7abee4eabb4708dec86db8d9ed0e09a5004
r.organ <r.organ@dan-it.com.ua>
2025-10-19 17:58:18 +0300

    connected db and employee resources

---
+++ src/database/database.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+}
--- src/types/users.ts
+++ src/types/users.ts
@@ -1,14 +1,1 @@
 export type Role = 'ADMIN' | 'ENGINEER' | 'INTERN';
-
-export type CreateUser = {
-  name: string;
-  email: string;
-  role: Role;
-};
-
-export type UpdateUser = {
-  id: number;
-  name?: string;
-  email?: string;
-  role?: Role;
-};
